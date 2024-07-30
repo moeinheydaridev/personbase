d6fd5cb2dbaca41a5d38ca776446b5a5ad00b21b
moein <mynh277@gmail.com>
2024-07-30 11:43:04 +0330

    Add project files.

---
+++ ConsoleApp4/StudentsInformation.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace student
+{
+    public class StudentsInformation
+    {
+       
+        public double Score1;
+        public double Score2;
+        public double Score3;
+
+        public StudentsInformation (double score1, double score2, double score3)
+        {
+            Score1 = score1;
+            Score2 = score2;
+            Score3 = score3;
+
+        }
+    }   
+
+    }
